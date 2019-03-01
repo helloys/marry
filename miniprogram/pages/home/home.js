f70c437d39358160f64d8b8d515e6e7e320ac75e
helloys <yushuang@bupt.edu.cn>
2019-03-01 18:00:35 +0800

    添加首页音乐播放 和  地图导航

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,3 +1,34 @@
-Page({
 
+var musicUrl = 'http://www.ytmp3.cn/down/49676.mp3'
+
+Page({
+  data: {
+    autoplay: true,
+    isPlayingMusic: false,
+    music_url: musicUrl
+  },
+
+  //生命周期函数--监听页面加载
+  onLoad: function (data) {
+    this.play()
+  },
+
+  play: function (event) {
+    if (this.data.isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic: false
+      })
+    } else {
+      console.log('this.data.music_url', this.data.music_url)
+      wx.playBackgroundAudio({
+        dataUrl: this.data.music_url,
+        title: '',
+        coverImgUrl: ''
+      })
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+  }
 })
--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -7,8 +7,8 @@ let inviteName = 'Mr.王&Miss.刘'
 let inviteDateOne = '谨定于 2018年8月18日'
 let inviteDateTwo = '农历 2018年七月初八 举办婚礼'
 let inviteAddress = '地址：沧州市盐山县庆云镇前簸箕村(点击导航)'
-let inviteLongitude = 117.398193
-let inviteLatitude = 37.895525
+let inviteLongitude = 111.900516
+let inviteLatitude = 30.480652
 
 Page({
   data: {
